d634604ad1e17c39122d1ae321c5097c8e8da508
SopapillaKat <vellita2011@gmail.com>
2022-10-13 16:39:32 -0700

    1stBuild

--- RPG/Assets/Scripts/Menu.cs
+++ RPG/Assets/Scripts/Menu.cs
@@ -61,6 +61,9 @@ public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
 
         // activate the reequested screen
         screen.SetActive(true);
+
+        if(screen == lobbyBrowserScreen)
+            UpdateLobbyBrowserUI();
     }
 
     public void OnBackButton()
@@ -142,5 +145,58 @@ public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
     {
         PhotonNetwork.LeaveRoom();
         SetScreen(mainScreen);
+    }
+
+        //LOBBY BROWSER SCREEN
+
+    GameObject CreateRoomButton()
+    {
+        GameObject buttonObj = Instantiate(roomButtonPrefab, roomListContainer.transform);
+        roomButtons.Add(buttonObj);
+
+        return buttonObj;
+    }
+
+    void UpdateLobbyBrowserUI()
+    {
+        // disabel all room buttons
+        foreach(GameObject button in roomButtons)
+            button.SetActive(false);
+
+        // display all current rooms in the master server
+        for(int x = 0; x < roomList.Count; ++x)
+        {
+            // get or create the button obj
+            GameObject button = x >= roomButtons.Count ? CreateRoomButton() : roomButtons[x];
+
+            button.SetActive(true);
+
+            // set the room name and player count text
+            button.transform.Find("RoomNameText").GetComponent<TextMeshProUGUI>().text = roomList[x].Name;
+            button.transform.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text = roomList[x].PlayerCount + " / " + roomList[x].MaxPlayers;
+
+            // set the button onclick event
+            Button buttonComp = button.GetComponent<Button>();
+
+            string roomName = roomList[x].Name;
+
+            buttonComp.onClick.RemoveAllListeners();
+            buttonComp.onClick.AddListener(() => { OnJoinRoomButton(roomName); });
+        }
+    }
+
+    public void OnJoinRoomButton(string roomName)
+    {
+        NetworkManager.instance.JoinRoom(roomName);
+    }
+
+    public void OnRefreshButton()
+    {
+        UpdateLobbyBrowserUI();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> allRooms)
+    {
+        roomList = allRooms;
     }
 }
