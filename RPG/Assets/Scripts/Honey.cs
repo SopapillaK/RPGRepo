2006e2ef3ad470e0dacdc825b0395b666494908e
SopapillaKat <vellita2011@gmail.com>
2022-10-27 11:51:52 -0700

    scda

---
+++ RPG/Assets/Scripts/Honey.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Honey : MonoBehaviourPun
+{
+    [Header("Info")]
+    public int curHp;
+    public int maxHp;
+    //public string objectToSpawnOnDeath;
+
+    [Header("Components")]
+    public SpriteRenderer sr;
+    GameObject honeyBlock;
+
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        curHp -= damage;
+
+        // update health bar
+        //healthBar.photonView.RPC("UpdateHealthBar", RpcTarget.All, curHp);
+
+        if(curHp <= 0)
+            Die();
+        else
+        {
+            photonView.RPC("FlashDamage", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void FlashDamage()
+    {
+        StartCoroutine(DamageFlash());
+
+            IEnumerator DamageFlash()
+            {
+                sr.color = Color.red;
+                yield return new WaitForSeconds(0.05f);
+                sr.color = Color.white;
+            }
+    }
+
+    void Die()
+    {
+        //if(objectToSpawnOnDeath != string.Empty)
+            //PhotonNetwork.Instantiate(objectToSpawnOnDeath, transform.position, Quaternion.identity);
+
+        // destroy object accross network
+        PhotonNetwork.Destroy(gameObject);
+    }
+}
